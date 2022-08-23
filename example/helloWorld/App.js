00c9114657e28dbde012cb4cdef75f25cacd2e5b
贺创 <2236402130@qq.com>
2022-08-23 12:19:30 +0800

    实现注册事件功能

--- example/helloWorld/App.js
+++ example/helloWorld/App.js
@@ -1,17 +1,33 @@
 import { h } from '../../lib/guide-mini-vue-esm.js'
 
+// const templateSetupResult = h('div', {}, 'hello,' + this.msg)
+// const templateArrayChildren = h('div', { class: 'root' }, [
+//     h('p', { class: 'red' }, this.msg),
+//     h('p', { class: 'green' }, 'mini-vue')
+// ])
+
 export const App = {
-  render () {
-    // return h('div',{}, 'hello,' + this.msg)
-    return h('div',{ class: 'root' }, [
-      h('p', { class: 'red' }, this.msg),
-      h('p', { class: 'green' }, 'mini-vue')
-    ])
-  },
-  setup () {
-    // composition api
-    return {
-      msg: 'mini-vue'
+    render() {
+        // return h('div',{}, 'hello,' + this.msg)
+        return h(
+            'div', {
+                class: 'root',
+                onClick() {
+                    console.log('click')
+                },
+                onMousedown() {
+                    console.log('mousedown')
+                }
+            }, [
+                h('p', { class: 'red' }, this.msg),
+                h('p', { class: 'green' }, 'mini-vue')
+            ]
+        )
+    },
+    setup() {
+        // composition api
+        return {
+            msg: 'mini-vue'
+        }
     }
-  }
 }
