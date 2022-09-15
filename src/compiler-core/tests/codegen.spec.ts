c14e9521c586bba2d16bd07482be7ca17a721047
贺创 <2236402130@qq.com>
2022-09-15 17:51:19 +0800

    生成string类型代码

--- src/compiler-core/tests/codegen.spec.ts
+++ src/compiler-core/tests/codegen.spec.ts
@@ -1,9 +1,11 @@
 import {baseParse} from "../src/parse";
 import {generate} from "../src/codegen";
+import {transform} from "../src/transform";
 
 describe('codegen.ts', () => {
   it('string', () => {
     const ast = baseParse('hi')
+    transform(ast)
     const { code } = generate(ast)
 
     expect(code).toMatchSnapshot()
