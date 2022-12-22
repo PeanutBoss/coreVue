3caf11da6595e95a9ec3f3d5b726a1185d5c116d
heChuang <2236402130@qq.com>
2022-12-23 07:18:27 +0800

    实现getCurrentInstance API

--- src/example-slots/App.js
+++ src/example-slots/App.js
@@ -1,35 +1,10 @@
-import { h, renderSlots, createTextVNode } from '../../lib/guide-mini-vue-esm.js'
-
-const Child = {
-  setup (props, { emit }) {
-    // props.count++
-    const emitAdd = (e) => {
-      e.stopPropagation()
-      emit('add', 'a', 'b', 'c')
-    }
-    const emitChange = (e) => {
-      e.stopPropagation()
-      emit('change', 1, 2, 3)
-    }
-    return {
-      emitAdd,
-      emitChange
-    }
-  },
-  render() {
-    const btn = h('button', {
-      onClick: this.emitAdd
-    }, 'add')
-    const change = h('button', {
-      onClick: this.emitChange
-    }, 'change')
-    const foo = h('div', {}, 'count = ' + this.count)
-    return h('div', {}, [foo, btn, change])
-  }
-}
+import { h, renderSlots, createTextVNode, getCurrentInstance } from '../../lib/guide-mini-vue-esm.js'
 
 const SlotComp = {
-  setup () {},
+  name: 'Slot',
+  setup () {
+    console.log(getCurrentInstance())
+  },
   render() {
     const age = 22
     const foo = h('div', {}, 'slotComp')
@@ -80,6 +55,7 @@ export default {
     )
   },
   setup () {
+    console.log(getCurrentInstance())
     return {
       msg: 'mini-vue'
     }
