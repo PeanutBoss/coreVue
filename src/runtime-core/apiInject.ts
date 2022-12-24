f656d9760d12003bb6eb81a0aaceac476bf03ca0
贺创 <2236402130@qq.com>
2022-12-24 16:52:47 +0800

    修复重写父组件provides的问题

--- src/runtime-core/apiInject.ts
+++ src/runtime-core/apiInject.ts
@@ -1,9 +1,18 @@
 import { getCurrentInstance } from './component'
 
 export function provide (key, value) {
-    const { provides }: any = getCurrentInstance()
-    provides[key] = value
-    console.log(key, value)
+    const currentInstance: any = getCurrentInstance()
+    const parentProvides = currentInstance.parent.provides
+    if (currentInstance) {
+        let { provides } = currentInstance
+        // 创建组件实例时：由 `provides: parent ? parent.provides : {}` 得出
+        // 如果当前组件的 provides 等于父组件的 provides，则说明当前组件的provides没有被初始化
+        if (provides === parentProvides) {
+            // 将父组件的provides作为当前组件provides的原型对象
+            provides = currentInstance.provides = Object.create(parentProvides)
+        }
+        provides[key] = value
+    }
 }
 
 export function inject (key) {
